d470e21644992b22451c9567fc9ace8979b1cca8
Siva <s.sisupalan@gingco.de>
2024-03-20 16:28:41 +0100

    [TASK] Add form parser

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write(
+      `<html>
+        <head>
+        <title>Hello</title>
+        </head>
+        <body>
+        <form method="POST" action="/message">
+            <input type="text" name="message" />
+            <input type="submit" />
+        </form>
+        </body>
+         </html>`
+    );
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split("=")[1];
+      fs.writeFileSync("mydata.txt", message);
+    });
+
+    res.writeHead("302", {
+      Location: "/",
+    });
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write(
+    "<html><head><title>Hello</title></head><body>Hello </body></html>"
+  );
+  res.end();
 });
 
 server.listen(3000);
