f766f7ab7314580f1fb47cac7ef0ee52d99c209b
Siva <s.sisupalan@gingco.de>
2024-03-20 16:59:45 +0100

    [TASK] Add new route files for better management

---
+++ routes.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+
+const reqHandler = (req, res) => {
+  if (url === "/") {
+    res.write(
+      `<html>
+              <head>
+              <title>Hello</title>
+              </head>
+              <body>
+              <form method="POST" action="/message">
+                  <input type="text" name="message" />
+                  <input type="submit" />
+              </form>
+              </body>
+               </html>`
+    );
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    return req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split("=")[1];
+      fs.writeFile("mydata.txt", message, (err) => {
+        res.writeHead("302", {
+          Location: "/",
+        });
+        return res.end();
+      });
+    });
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write(
+    `<html>
+          <head>
+          <title>Hello</title></head>
+          <body>Hello </body>
+          </html>`
+  );
+  res.end();
+};
+
+module.exports = reqHandler;
