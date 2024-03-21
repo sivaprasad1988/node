8d0266ea7b2231a2e3f3515ed5e954b3f56abca6
Siva <s.sisupalan@gingco.de>
2024-03-21 17:42:19 +0100

    Express implementaion

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extends: false }));
 
 app.use("/", (req, res, next) => {
   console.log("This always runs!");
   next();
 });
 
-app.use("/add-product", (req, res, next) => {
+app.get("/add-product", (req, res, next) => {
   console.log("In another middleware!");
-  res.send('<h1>The "Add Product" Page</h1>');
+  res.send(`
+  <form method="POST" action="/products">
+    <input type="text" name="product" />
+    <input type="submit" />
+  </form>
+  `);
+});
+app.post("/products", (req, res, next) => {
+  console.log("In  products!");
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
--- assignment-21-03-2024/app.js
+++ assignment-21-03-2024/app.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("/");
-  res.send("/index");
-});
-
 app.use("/users", (req, res, next) => {
   console.log("/users");
   res.send("/users");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("/");
+  res.send("/index");
 });
 
 app.listen(3000);
